f74ef000d9aa18e15d851fe59f6e523add608f96
midimitrova <mariyapopova95@gmail.com>
2022-12-14 22:14:56 +0200

    Add Day 16 - Object-Oriented Programming

---
+++ 100_Days_of_Code_The_Complete_Python_Pro_Bootcamp_for_2023/Intermediate/Section_16_OOP/oop-coffee-machine-start/main.py
@@ -0,0 +1,34 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+coffee_maker.report()
+money_machine.report()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+
+    choice = input(f'What would you like? {options}: ').lower().strip()
+
+    if choice == 'off':
+        is_on = False
+    elif choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    elif choice not in options:
+        print('Invalid input! Please, choose again!')
+        continue
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+
+
