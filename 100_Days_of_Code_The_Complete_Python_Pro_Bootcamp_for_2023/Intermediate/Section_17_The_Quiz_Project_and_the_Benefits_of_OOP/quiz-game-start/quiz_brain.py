7f571f7a67d15c19f35068b1fbbeff60e9bd68ff
midimitrova <mariyapopova95@gmail.com>
2022-12-30 15:22:42 +0200

    Add Day 17 - The Quiz Project

---
+++ 100_Days_of_Code_The_Complete_Python_Pro_Bootcamp_for_2023/Intermediate/Section_17_The_Quiz_Project_and_the_Benefits_of_OOP/quiz-game-start/quiz_brain.py
@@ -0,0 +1,29 @@
+class QuizBrain:
+
+    def __init__(self, question_list):
+        self.question_list = question_list
+        self.question_number = 0
+        self.score = 0
+
+    def still_has_questions(self):
+        number_of_questions = len(self.question_list)
+        if self.question_number + 1 > number_of_questions:
+            return False
+        return True
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f'Q.{self.question_number}: {current_question.question} (True/False): ').strip()
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print('You got it right!')
+
+        else:
+            print('That\'s wrong.')
+            print(f'The correct answer was: {correct_answer}.')
+        print(f'Your current score is: {self.score}/{self.question_number}.')
+        print()
